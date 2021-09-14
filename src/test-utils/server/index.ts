e5944fc62b62e0f38971c296eecf208cd547a458
Dewaun Ayers <dewaun.ayers@gmail.com>
2021-09-15 09:39:11 +1000

    remove unsued import/export

--- src/test-utils/server/index.ts
+++ src/test-utils/server/index.ts
@@ -1,7 +1,7 @@
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
-import { handlers, API_URL, fixtures } from './handlers';
+import { handlers, API_URL } from './handlers';
 
 const server = setupServer(...handlers);
 
-export { rest, server, API_URL, fixtures };
+export { rest, server, API_URL };
